77724ff07d808e459763a74963fac63b3691c2cb
Anand Mukut Tirkey <anand@anands-macbook-pro.local>
2017-02-22 21:18:33 +0530

    Initial Commit

---
+++ tesserectOCR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tesserectOCR
+//
+//  Created by Anand Mukut Tirkey on 22/02/17.
+//  Copyright © 2017 anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
